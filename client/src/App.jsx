8bfabc2ff47dbc667db39d7f97975c77218442e0
ab-0120 <anshitabisht01@gmail.com>
2026-08-10 11:33:57 +0530

    Made the fized whatsapp and call icon that appears on every page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import Navbar from "./components/Navbar";
+import FloatingIcons from "./components/FloatingIcons";
+import { Route, Routes } from "react-router-dom";
 
 function App(){
   return(
     <div>
       <Navbar />
+      <FloatingIcons />
     </div>
   )
 }
